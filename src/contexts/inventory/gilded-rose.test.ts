68ffabe7a1b050fcb534c43d8e0c2f1b5311f2a6
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2023-05-09 19:38:50 +0300

    test: quality should never be negative

--- src/contexts/inventory/gilded-rose.test.ts
+++ src/contexts/inventory/gilded-rose.test.ts
@@ -1,6 +1,34 @@
+import { GildedRose, Item } from './gilded-rose';
+
+// Helper function to create an item
+// This is just to make the tests more readable
+const createItem = (name: string, properties: { sellIn: number, quality: number }) => {
+  return new Item(name, properties.sellIn, properties.quality);
+};
+
 describe('GildedRose class functionality', () => {
+  let gildedRose: GildedRose;
+
+  beforeEach(() => {
+    gildedRose = new GildedRose();
+  });
+
   describe('General', () => {
-    // General tests which apply to all items in the inventory
+    test('Quality should never be negative', () => {
+      const item = createItem('Test Item', {
+        sellIn: 10,
+        quality: 1
+      });
+      gildedRose.items = [item];
+
+      // Quality should decrease by 1
+      gildedRose.updateQuality();
+      expect(gildedRose.items[0].quality).toBe(0);
+
+      // Quality should not decrease below 0
+      gildedRose.updateQuality();
+      expect(gildedRose.items[0].quality).toBe(0);
+    });
   });
 
 
