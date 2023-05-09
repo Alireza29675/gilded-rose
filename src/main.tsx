0066998935a876a0b0788e04fdd32dab56da64f6
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2023-05-09 19:22:32 +0300

    feat: jest init

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App.tsx'
 
-ReactDOM.createRoot(document.querySelector('#root')!).render(
+const root = document.querySelector('#root') as HTMLDivElement;
+
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
