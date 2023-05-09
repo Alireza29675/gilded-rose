9403929d3f7e9684abe1d82976b915c2c38ffbeb
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2023-05-09 20:43:12 +0300

    refactor: aged-brie and sulfuras tests pass

--- src/contexts/inventory/gilded-rose.ts
+++ src/contexts/inventory/gilded-rose.ts
@@ -1,3 +1,6 @@
+import { ItemType } from "./types";
+import getItemType from "./utils/getItemType";
+
 export class Item {
   name: string;
   sellIn: number;
@@ -18,9 +21,58 @@ export class GildedRose {
   }
 
   updateQuality() {
-    return this._updateQuality();
+    for (const item of this.items) {
+      this.updateItem(item)
+    }
   }
 
+  private updateItem(item: Item) {
+    const type = getItemType(item.name);
+
+    // Sulfuras is a legendary item, therefore we don't need to update anything 🔥
+    if (type === ItemType.Sulfuras) return;
+
+    switch (type) {
+      case ItemType.AgedBrie:
+        this.updateAgedBrie(item);
+        break;
+      case ItemType.BackstagePasses:
+        this.updateBackstagePasses(item);
+        break;
+      case ItemType.Conjured:
+        this.updateConjured(item);
+        break;
+      default:
+        this.updateRegularItem(item);
+    }
+
+    // All items except Sulfuras have a sellIn value, which we need to update
+    item.sellIn--;
+
+    // Quality can never be negative
+    item.quality = Math.max(0, item.quality);
+
+    // Quality can never be more than 50
+    item.quality = Math.min(50, item.quality);
+  }
+
+  private updateAgedBrie(item: Item) {
+    item.quality++;
+  }
+
+  private updateBackstagePasses(_: Item) {
+    // update backstage passes
+  }
+
+  private updateConjured(_: Item) {
+    // update conjured
+  }
+
+  private updateRegularItem(_: Item) {
+    // update regular item
+  }
+
+  /*
   private _updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
@@ -70,4 +122,5 @@ export class GildedRose {
 
     return this.items;
   }
+  */
 }
